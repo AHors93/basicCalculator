99b5dd92549599ccd0877556a21ffb9cd9de8ed0
Adam Horscraft <Adam@Adams-MacBook-Pro-2.local>
2020-07-28 12:32:36 +0100

    Updates

---
+++ precache-manifest.c921a255a4c484ba79dbf037e45b35bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c548437102e49cf6f25d0ccd41be47d7",
+    "url": "/basicCalculator/index.html"
+  },
+  {
+    "revision": "3669fe51c7fc9f5b5314",
+    "url": "/basicCalculator/static/css/main.7e13edf3.chunk.css"
+  },
+  {
+    "revision": "6039d017c167b6337e9d",
+    "url": "/basicCalculator/static/js/2.c97a4f24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basicCalculator/static/js/2.c97a4f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3669fe51c7fc9f5b5314",
+    "url": "/basicCalculator/static/js/main.07464e80.chunk.js"
+  },
+  {
+    "revision": "6cf9ab1c40e024773e6b",
+    "url": "/basicCalculator/static/js/runtime-main.9f87a4a3.js"
+  }
+]);
