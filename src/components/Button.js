38bf9ffc6ea98655369938e7bf1ca36672ecc8c7
Adam Horscraft <Adam@Adams-MacBook-Pro-2.local>
2020-07-28 10:16:33 +0100

    this is my commit comment

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Button.css";
+
+class Button extends React.Component {
+    isOperator = val => {
+        return !isNaN(val) || val === "." || val === "=";
+    };
+
+    render() {
+        return (
+            <div
+                className={`button ${
+                    this.isOperator(this.props.children) ? "" : "operator"
+                    }`}
+                onClick={() => this.props.handleClick(this.props.children)}
+            >
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default Button;
